a696bb286698714540945deb267dc0bd00fd2a51
GideonVermeulen <63670713+GideonVermeulen@users.noreply.github.com>
2025-08-30 13:17:41 +0200

    added the ability so solve min problems

--- obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f6641e4cf11f6aaeaf989faabe4706c2de93c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6878cc60a4fce27d850de67e81e916bcdd6e7260")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
